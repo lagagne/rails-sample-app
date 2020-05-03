f1dd739a976ddf78b8aab83a5d344e9adbacc4d1
lagagne <louisandregagne@gmail.com>
2020-05-03 16:04:46 -0400

    Seed sample users

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,7 @@
+#Rails 6.0.2.2 makes too many deprecated funtion calls to Ruby 2.7.1
+#Remove if necessary
+$VERBOSE=nil
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
