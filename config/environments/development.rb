692f8f6fde8f936117d51f6ea1bebcab762ad921
lagagne <louisandregagne@gmail.com>
2020-05-06 14:46:01 -0400

    Configure email previews

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,10 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  host = 'localhost:3000'
+  # Use this on the cloud IDE.
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
